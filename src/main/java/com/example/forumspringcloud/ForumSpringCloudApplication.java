58889b0bc8eb7df15d45f2e8db4aa9d2b2691bca
VH992098059 <992098059@qq.com>
2024-03-07 10:37:11 +0800

    论坛项目

---
+++ src/main/java/com/example/forumspringcloud/ForumSpringCloudApplication.java
@@ -0,0 +1,14 @@
+package com.example.forumspringcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class ForumSpringCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForumSpringCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/forumspringcloud/ForumSpringCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.forumspringcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumSpringCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
